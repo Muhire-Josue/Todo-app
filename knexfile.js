8b62e0fdd2c86550f1adec699bc7fe7d34ef4a87
Muhire-Josue <rutayisire.josue09@gmail.com>
2019-12-11 21:21:52 +0200

    chore ( create folder structure): add folder structure
    	- add all folders
    	- add all files in root dir

---
+++ knexfile.js
@@ -0,0 +1,9 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://postgres:root@localhost:5432/todo_dev',
+  },
+};
